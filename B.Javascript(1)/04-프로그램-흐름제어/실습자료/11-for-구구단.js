167a013a4e5b68148aa3e07fb4a69426b9b0d88f
cheon1217 <friendly1217@naver.com>
2023-09-21 00:29:15 +0900

    숙제

--- B.Javascript(1)/04-프로그램-흐름제어/실습자료/11-for-구구단.js
+++ B.Javascript(1)/04-프로그램-흐름제어/실습자료/11-for-구구단.js
@@ -4,3 +4,8 @@ for (let x = 1; x <= 9; x++) {
         console.log("%d X %d = %d", x, y, z);
     }
 }
+
+for (let y = 1; y < 10; y++) {
+    const z = y * 7;
+    console.log("7 x %d = %d", y, z);
+}
---
+++ B.Javascript(1)/04-프로그램-흐름제어/실습자료/14-for-2감소.js
@@ -0,0 +1,3 @@
+for (let a = 10; a > 0; a -= 2) {
+    console.log("a=%d", a);
+}
