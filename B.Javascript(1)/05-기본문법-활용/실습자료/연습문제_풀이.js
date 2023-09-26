63d8e140e12dcc1a66f229dec0328dc3f659d901
cheon1217 <friendly1217@naver.com>
2023-09-26 21:27:49 +0900

    9월 26일 수업

--- B.Javascript(1)/05-기본문법-활용/실습자료/연습문제_풀이.js
+++ B.Javascript(1)/05-기본문법-활용/실습자료/연습문제_풀이.js
@@ -118,3 +118,54 @@ for (let i=0; i<7; i++) {
 
     console.log(str);
 }
+
+/** 문제 7 */
+let number = 2;
+
+// case 1 : if-for
+if (number == 1) {
+    for (let i = 3; i < 10; i += 2) {
+        for (let j = 1; j < 10; j++) {
+            console.log("%d X %d = %d", i, j, i*j);
+        }
+    }
+} else {
+    for (let i = 2; i < 10; i += 2) {
+        for (let j = 1; j < 10; j++) {
+            console.log("%d X %d = %d", i, j, i*j);
+        }
+    }
+}
+
+// case 2 : for-if
+for (let i = 2; i < 10; i++) {
+    if (number == 1) {
+        if (i % 2 == 1) {
+            for (let j = 1; j < 10; j++) {
+                console.log("%d X %d = %d", i, j, i*j);
+            }
+        }
+    } else {
+        if (i % 2 == 0) {
+            for (let j = 1; j < 10; j++) {
+                console.log("%d X %d = %d", i, j, i*j);
+            }
+        }
+    }
+}
+
+// case 3
+let start = number == 2 ? 2 : 3;
+
+for (let i = start; i < 10; i+=2) {
+    for (let j = 1; j < 10; j++) {
+        console.log("%d X %d = %d", i, j, i*j);
+    }
+}
+
+// case 4
+for (let i = 4-number; i < 10; i+=2) {
+    for (let j = 1; j < 10; j++) {
+        console.log("%d X %d = %d", i, j, i*j);
+    }
+}
