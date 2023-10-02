f4c170be002f214ea60d40d5ec605b2dc45fea95
cheon1217 <friendly1217@naver.com>
2023-10-02 16:54:16 +0900

    공부

--- B.Javascript(1)/07-JSON/실습자료/09-구조분해.js
+++ B.Javascript(1)/07-JSON/실습자료/09-구조분해.js
@@ -23,3 +23,32 @@ const arr = [100, 200];
 const [x, y] = arr;
 console.log(x); // 100
 console.log(y); // 200
+
+// 구조분해를 활용하여 필요한 데이터만 추출하기
+const data = { name:'hello', age:20, height:172, weight:85 };
+const {name} = data;
+console.log(name);
+
+// data안에 있는 height와 weigth를 분해하면서 이름을 h 와 w롤 변경
+const {height: h, weight: w} = data;
+console.log(h);
+console.log(w);
+console.log(data);
+
+// 구조분해를 수행한 나머지를 별도로 분리하기
+// --> 'rest_b'라는 것은 단순한 변수 이름이므로 어떤 단어를 사용해도 무관
+const dummy = { a1: 100, a2: 200, a3: 300, a4: 400 };
+const {a1, a2, ... rest_b} = dummy;
+console.log(a1);
+console.log(a2);
+console.log(rest_b);
+
+// 구조분해를 활용하여 기존 데이터와 추가적인 값을 병합한 새로운 객체 생성
+// --> '...' 뒤에 오는 변수명은 반드시 원본 객체 이름이어야 한다.
+const origin = { name: 'javascript' , age: 25 };
+const newdata1 = {...origin, gender: 'M'};
+console.log(newdata1);
+
+// 구조분해를 통한 새로운 데이터 생성시 원본과 동일한 이름의 속성이 있다면 원본 데이터를 수정한다.
+const newdata2 = {...origin, age: 30, gender: 'F'};
+console.log(newdata2);
