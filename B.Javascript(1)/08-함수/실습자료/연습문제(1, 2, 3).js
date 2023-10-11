e79fb0c99ca56f1a74c9d9d21b1035d11c535d2c
cheon1217 <friendly1217@naver.com>
2023-10-11 14:50:22 +0900

    10월 11일 공부

--- B.Javascript(1)/08-함수/실습자료/연습문제(1, 2, 3).js
+++ B.Javascript(1)/08-함수/실습자료/연습문제(1, 2, 3).js
@@ -36,30 +36,63 @@ function printRevStar(max) {
 
 printRevStar(5);
 
-/** 문제 3 */
-function myGame(n) {
+/** 문제 3 틀림 */
+// function myGame(n) {
     
+//     for (let i=1; i<=n; i++) {
+
+//         const location10 = (i - (i % 10)) / 10;;
+//         const location1 = i % 10;
+        
+//         if (location10 == 3 || location10 == 6 || location10 == 9) {
+//             if (location1 == 3 || location1 == 6 || location1 == 9) {
+//                 console.log("짝짝(" + i + ") --> 2번");
+//             } else {
+//                 console.log("짝(" + i + ") --> 1번");
+//             }
+//         } else {
+//             if (location1 == 3 || location1 == 6 || location1 == 9) {
+//                 console.log("짝(" + i + ") --> 1번");
+//             } else {
+//                 console.log(i);
+//             }
+//         }
+
+//     }
+
+// }
+
+/** 문제 3 복습 */
+function myGame(n) {
+
+    let count = 0;
+
     for (let i=1; i<=n; i++) {
 
-        const location10 = (i - (i % 10)) / 10;;
-        const location1 = i % 10;
-        
-        if (location10 == 3 || location10 == 6 || location10 == 9) {
-            if (location1 == 3 || location1 == 6 || location1 == 9) {
-                console.log("짝짝(" + i + ") --> 2번");
-            } else {
-                console.log("짝(" + i + ") --> 1번");
+        const str = i + "";
+
+        let say = "";
+
+        let clap = 0;
+
+        for (let s of str) {
+            if (s == "3" || s == "6" || s == "9") {
+                say += "짝";
+                clap++;
             }
+        }
+
+        if (clap == 0) {
+            console.log(i);
         } else {
-            if (location1 == 3 || location1 == 6 || location1 == 9) {
-                console.log("짝(" + i + ") --> 1번");
-            } else {
-                console.log(i);
-            }
+            console.log(`${say} (${i}) --> ${clap}`);
+            count += clap;
         }
 
     }
 
+    return count;
+
 }
 
-myGame(35);
+console.log("박수를 총 %d번 쳤습니다.", myGame(35));
--- B.Javascript(1)/08-함수/실습자료/연습문제(4, 5).js
+++ B.Javascript(1)/08-함수/실습자료/연습문제(4, 5).js
@@ -2,35 +2,35 @@
 // max는 최대 행 수, current는 현재 출력중인 행의 위치
 function printStar(max, current=1) {
 
-    if (current > (max+1)) {
+    if (current > max) {
         return;
     } else {
-        for (let i=current; i<=max; i++) {
-            let star = "";
-            for (let j=current; j<=i; j++) {
+        let star = "";
+        for (let i=0; i<current; i++) {
                 star += "*";
             }
-            console.log(star);
-        }
+        console.log(star);
+        printStar(max,current+1);
     }
 
 }
 
 printStar(5);
 
+console.log();
+
 /** 문제 5 */
 function printRevStar(max, current=1) {
 
-   if (current > (max+1)) {
+   if (current > max) {
         return;
    } else {
-        for (let i=current; i<=max; i++) {
-            let star = "";
-            for (let j=current; j<=max-i+1; j++) {
-                star += "*";
-            }
-            console.log(star);
+       let star = "";
+        for (let i=0; i<max-current+1; i++) {
+            star += "*";
         }
+        console.log(star);
+        printRevStar(max,current+1);
    }
 
 }
