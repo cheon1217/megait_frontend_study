accc9bfdce9f682b6bbae861ed29b1bfc046c94c
cheon1217 <friendly1217@naver.com>
2023-11-21 14:52:02 +0900

    공부

--- D.ReactJS/03-jsx/src/pages/If4.js
+++ D.ReactJS/03-jsx/src/pages/If4.js
@@ -1,9 +1,23 @@
 import React from "react";
 
+/**
+ * jsx 조건분기 (4) - 삼항 연산자를 사용한 조건 분기
+ * {조건 ? (조건이 참인 경우 출력할 내용) : (그렇지 않은 경우 출력할 내용)}
+ * 조건이 거짓은 경우를 사용하지 않고자 한다면 null 사용
+ * ex) { point === 80 ? ( ... ) : (null) }
+ */
 const If4 = () => {
+    const isLogin = false;
+
     return (
         <div>
             <h1>If4 페이지</h1>
+
+            {
+                isLogin === true ?
+                <button type="button">로그아웃</button> :
+                <button type="button">로그인</button>
+            }
         </div>
     );
 };
