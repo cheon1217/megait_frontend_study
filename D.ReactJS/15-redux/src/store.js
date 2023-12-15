3383256885d4a8069f287e81f3103cfd3f0628cf
김희재 <test@test.com>
2023-12-15 21:35:46 +0900

    12월 15일  수업

---
+++ D.ReactJS/15-redux/src/store.js
@@ -0,0 +1,13 @@
+import { configureStore } from "@reduxjs/toolkit";
+import CounterSlice from "./slices/CounterSlice";
+
+const store = configureStore({
+    // 개발자가 직접 작성한 Slice 오브젝트들이 명시되어야한다.
+    reducer: {
+        CounterSlice: CounterSlice
+    }
+    // 비동기 미들웨어 추가 (Ajax처리가 필요한 경우만 설정)
+    // middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false })
+});
+
+export default store;
