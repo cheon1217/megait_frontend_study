4d1ce2aec87ffc196050e8e1fc0b75c5c99da46d
김희재 <test@test.com>
2023-12-18 21:30:36 +0900

    12월 18일 수업

--- D.ReactJS/15-redux/src/App.js
+++ D.ReactJS/15-redux/src/App.js
@@ -3,6 +3,8 @@ import { Routes, Route } from "react-router-dom";
 
 import MenuLink from "./components/MenuLink";
 import Counter from "./pages/Counter";
+import Department from "./pages/Department";
+import News from "./pages/News";
 
 
 const App = memo(() => {
@@ -11,10 +13,14 @@ const App = memo(() => {
             <h1>15-redux</h1>
             <nav>
                 <MenuLink to="/counter">Counter</MenuLink>
+                <MenuLink to="/department">Department</MenuLink>
+                <MenuLink to="/news">News</MenuLink>
             </nav>
             <hr />
             <Routes>
                 <Route path="/counter" element={<Counter />} />
+                <Route path="/department" element={<Department />} />
+                <Route path="/news" element={<News />} />
             </Routes>
         </div>
     );
---
+++ D.ReactJS/15-redux/src/pages/MovieRank.js
@@ -0,0 +1,19 @@
+import React, { memo } from "react";
+
+import styled from "styled-components";
+
+const Container = styled.div`
+    
+`;
+
+const MovieRank = memo(() => {
+
+
+    return (
+        <div>
+            <h1></h1>
+        </div>
+    );
+});
+
+export default MovieRank;
