ac0de67a568e30b4aae82cf14c82864698026a51
cheon1217 <friendly1217@naver.com>
2024-01-03 15:20:24 +0900

    프로젝트 진행

--- D.ReactJS/mysite/src/common/Header.js
+++ D.ReactJS/mysite/src/common/Header.js
@@ -2,9 +2,11 @@ import * as colors from "../styles/colors";
 
 import React, { memo } from "react";
 
+import banner from "../assets/banner.png";
 import styled from "styled-components";
 
 const HeaderContainer = styled.div`
+    /* first banner */
     position: absolute;
     left: 0;
     right: 0;
@@ -25,6 +27,44 @@ const HeaderContainer = styled.div`
         .shp-logo {
             flex: none;
             margin-right: 26px;
+        }
+
+        .aside {
+            margin-left: auto;
+            padding-left: 280px;
+        }
+
+        .shop, .util {
+            position: relative;
+            margin-right: 24px;
+
+            a {
+                flex: none;
+                margin-right: 24px;
+            }
+
+            &::after {
+                position: absolute;
+                right: 0;
+                top: 50%;
+                transform: translateY(-50%);
+                display: inline-block;
+                content: "";
+                width: 1px;
+                height: 12px;
+                background: #555;
+            }
+        }
+
+        .nav {
+            a:first-child {
+                margin-left: 0;
+            }
+
+            a {
+                flex: none;
+                margin-left: 24px;
+            }
         }
     }
 
@@ -48,17 +88,59 @@ const HeaderContainer = styled.div`
     a {
         text-decoration: none;
     }
+
+    img {
+        width: 100%;
+        object-fit: cover;
+    }
+
+    /* second banner */
+    .second-area {
+        width: 100%;
+        border-bottom: 1px solid #000;
+
+        .second-wrap {
+            height: 70px;
+            background-color: #fff;
+
+            .inner {
+                display: flex;
+                justify-content: flex-start;
+                align-items: center;
+            }
+        }
+
+        .brand-title {
+            flex: none;
+            font-size: 24px;
+            line-height: 30px;
+            color: #111111;
+            font-weight: 700;
+
+            a {
+                display: flex;
+                align-items: center;
+                max-height: 40px;
+                overflow: hidden;
+            }
+
+            img {
+                max-height: 100%;
+            }
+        }
+    }
 `;
 
 const Header = memo(() => {
     return (
         <HeaderContainer>
+            {/* first banner */}
             <div class="top-area">
             {/*  알림 코너 영역 */}
             <div class="inner">
                 <h1 class="shp-logo">
-                    <a href="/main">
-                        <img src="" alt="" />
+                    <a href="#">
+                        <img src="" alt="MYSITE" />
                     </a>
                 </h1>
                 <div class="aside">
@@ -67,10 +149,10 @@ const Header = memo(() => {
                                 <img src="#" alt="ShoppingMall" />
                             </a>
                             <a href="#">
-                                <img src="#" alt="BEAKER" />
+                                <img src="#" alt="SAMSUNG" />
                             </a>
                             <a href="#">
-                                <img src="#" alt="ANOTHER#" />
+                                <img src="#" alt="APPLE" />
                             </a>
                         </div>
                         <div class="util">
@@ -87,8 +169,19 @@ const Header = memo(() => {
                         </div>
                 </div>
             </div>
+
+            {/* second banner */}
             <div className="second-area">
-                
+                <div className="second-wrap">
+                    <div className="inner">
+                        <h2 className="brand-title">
+                            <a href="#" id="brandLogo">
+                                <img src="" alt="8 seconds" />
+                            </a>
+                        </h2>
+                        <a href="#"><i class="fa-regular fa-heart"><span>9999+</span></i></a>
+                    </div>
+                </div>
             </div>
         </HeaderContainer>
     )
