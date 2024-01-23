456c2cbb3fb6aa7fe188eb70d01423dd2a3e1159
김희재 <test@test.com>
2024-01-23 21:19:02 +0900

    1월 23일 프로젝트 진행

---
+++ E.NextJS/shopping-mall/src/pages/ranking/[product].js
@@ -0,0 +1,19 @@
+import React, { memo } from "react";
+
+import styled from "styled-components";
+
+const rankingContainer = styled.div`
+    
+`;
+
+const ranking = memo(() => {
+    
+
+    return (
+        <div>
+            <h1>[product]</h1>
+        </div>
+    );
+});
+
+export default ranking;
